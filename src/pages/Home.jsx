38b1044fbbc32da5a4a84c4c41b27c608cec35e4
Firdan Mildani <firdhanmildani@gmail.com>
2023-12-31 07:42:10 +0700

    update: add cv

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,32 +11,42 @@ import { Parallax } from "react-parallax";
 
 const Home = () => {
     useEffect(() => {
-        // Function to scroll to the section if the URL hash is present
         const scrollToSection = () => {
           const hash = window.location.hash;
           if (hash) {
-            const sectionId = hash.substring(1); // Remove the '#' symbol
+            const sectionId = hash.substring(1); 
             const section = document.getElementById(sectionId);
             if (section) {
               section.scrollIntoView({ behavior: 'smooth' });
             }
           }
         };
-    
-        // Scroll to section if the URL hash is present on initial load
+
         scrollToSection();
-    
-        // Listen for hash changes and scroll to the section accordingly
         window.addEventListener('hashchange', scrollToSection);
     
         return () => {
           window.removeEventListener('hashchange', scrollToSection);
         };
       }, []);
+
+      const handleDownload = () => {
+        
+        const cvUrl = process.env.PUBLIC_URL + '/files/CV-Firdan-Mildani.pdf';
+    
+        const link = document.createElement('a');
+        link.href = cvUrl;
+        link.setAttribute('download', 'CV-Firdan-Mildani.pdf'); 
+    
+        document.body.appendChild(link);
+        link.click();
+    
+        document.body.removeChild(link);
+      };
   return (
     <>
     <div className="hero">
-      <Parallax className="hero-img" strength={100} bgImage="img/banner2.jpg">
+      <Parallax className="hero-img" strength={300} bgImage="img/banner2.jpg">
           <div className="hero-content-wrapper">
           </div>
       </Parallax>    
@@ -60,7 +70,7 @@ const Home = () => {
                 </div>
                 <div className="row col-sm-3 col-12">
                     <div className='col-sm-12 col-6 text-center icon-menu'>
-                        <a href="/">
+                        <a href="/" onClick={handleDownload}>
                             <i class="bi bi-file-earmark custom-icon"></i>
                             <h5>CV</h5>
                             <p>Download CV</p>
@@ -80,52 +90,48 @@ const Home = () => {
     <div id='portfolio' className="section">
         <div className="container">
             <div className="row">
-            <div className="col-xs-12 col-md-12 col-md-centered col-lg-12">
-            <div className="section-title text-center mb-5">
-                <div className="section-title">
-                    <h1>PORTFOLIO</h1>
+                <div className="col-xs-12 col-md-12 col-md-centered col-lg-12">
+                    <div className="section-title text-center mb-5">
+                        <div className="section-title">
+                            <h1>PORTFOLIO</h1>
+                        </div>
+                        <div className="section-subtitle">
+                            <h6>Portfolio Showcase, A Glimpse into Creativity</h6>
+                        </div>
+                    </div>
+                    {portfolioItems.reduce((rows, item, index) => {
+                            if (index % 2 === 0) {
+                                rows.push(
+                                    <div className="section__row row row-thin" key={index}>
+                                        <PortfolioItem
+                                            _id={item._id}
+                                            title={item.title}
+                                            brief={item.brief}
+                                            imageSrc={item.imageSrc}
+                                            isLarge={index % 4 === 0 || index % 4 === 3}
+                                        />
+                                        {index + 1 < portfolioItems.length && (
+                                            <PortfolioItem
+                                                _id={portfolioItems[index + 1]._id}
+                                                title={portfolioItems[index + 1].title}
+                                                brief={portfolioItems[index + 1].brief}
+                                                imageSrc={portfolioItems[index + 1].imageSrc}
+                                                isLarge={(index + 1) % 4 === 0 || (index + 1) % 4 === 3}
+                                            />
+                                        )}
+                                    </div>
+                                );
+                            }
+                            return rows;
+                        }, 
+                    [])}
                 </div>
-                <div className="section-subtitle">
-                    <h6>Portfolio Showcase, A Glimpse into Creativity</h6>
-                </div>
             </div>
-            {portfolioItems.reduce((rows, item, index) => {
-                    if (index % 2 === 0) {
-                        rows.push(
-                            <div className="section__row row row-thin" key={index}>
-                                <PortfolioItem
-                                    _id={item._id}
-                                    title={item.title}
-                                    brief={item.brief}
-                                    imageSrc={item.imageSrc}
-                                    isLarge={index % 4 === 0 || index % 4 === 3}
-                                />
-                                {index + 1 < portfolioItems.length && (
-                                    <PortfolioItem
-                                        _id={portfolioItems[index + 1]._id}
-                                        title={portfolioItems[index + 1].title}
-                                        brief={portfolioItems[index + 1].brief}
-                                        imageSrc={portfolioItems[index + 1].imageSrc}
-                                        isLarge={(index + 1) % 4 === 0 || (index + 1) % 4 === 3}
-                                    />
-                                )}
-                            </div>
-                        );
-                    }
-                    return rows;
-                }, [])}
-            </div>
-            </div>
-            </div>
+        </div>
     </div>
     <div className="hero1">
       <Parallax className="hero-img" strength={300} bgImage="img/banner.jpg">
           <div className="hero-content-wrapper">
-              {/* <div className="hero-content">
-                  <span>Welcome to Vanillatelle!</span>
-                  <h1>Savor the Essence of Vanilla with Vanillatelle</h1>                
-                  <a href="products" className="button">Our Products</a>
-              </div> */}
           </div>
       </Parallax>    
     </div>
@@ -135,9 +141,6 @@ const Home = () => {
                 <div className="section-title">
                     <h1>SKILLS & EXPERIENCES</h1>
                 </div>
-                {/* <div className="section-subtitle">
-                <h6>Portfolio Showcase, A Glimpse into Creativity</h6>
-                </div> */}
             </div>
             <div className="row d-flex justify-content-center">
                 <div className="col-lg-5 col-md-6 col-12 mb-5">
@@ -187,8 +190,7 @@ AND WOULD LOVE TO HEAR FROM YOU</h6>
                         <small class="contact">
                             <a href="mailto:firdanmildani@gmail.com">firdanmildani@gmail.com</a>
                       <br/>
-                      Sukabumi / Indonesia<br/>
-
+                      Sukabumi / Jawa Barat / Indonesia<br/>
                       <a target="_blank" href="https://www.linkedin.com/in/firdanmildani/" rel="noreferrer">LinkedIn</a> / <a target="_blank" href="https://github.com/firdanmildani" rel="noreferrer">GitHub</a> / <a target="_blank" href="https://instagram.com/firdanmildani" rel="noreferrer">Instagram</a>
                     </small>
                     </div>
